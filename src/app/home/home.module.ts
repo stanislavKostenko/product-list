aaae8ffa67a44d34a7b01750d08d124c1b0ac657
stanislavKostenko <kostenko0407@gmail.com>
2018-06-15 18:37:29 +0300

    added spec files

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FilterComponent } from '../filter/filter.component';
+import { HomeComponent } from './home.component';
+import { CanDeactivateGuardService } from '../shared/can-deactivate-guard.service';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
+
+const categoryChildrenRoute: Routes = [
+  {path: ':name', component: CategoryComponent},
+  {path: 'all', component: CategoryComponent},
+];
+
+const routes: Routes = [
+  {path: 'about', loadChildren: './about/about.module#AboutModule'},
+  {
+    path: 'categories',
+    component: HomeComponent,
+    children: categoryChildrenRoute,
+    canDeactivate: [CanDeactivateGuardService]
+  },
+
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [HomeComponent, FilterComponent],
+  exports: [HomeComponent, FilterComponent]
+})
+export class HomeModule { }
+
